c6e905e166cd6b4ed3c3228777a1e5c87fbf7253
Kying-star <3077006505@qq.com>
2023-03-24 14:00:43 +0800

    feat: set Monaco width -> 736px

--- src/pages/Studio/index.tsx
+++ src/pages/Studio/index.tsx
@@ -9,7 +9,7 @@ type Props = {
 export const Studio: React.FC<Props> = (props: Props) => {
   const { code, setCode } = props;
   const [height, setHeight] = useState<number>(0);
-  const [width, setWidth] = useState<number>(782);
+  const [width, setWidth] = useState<number>(736);
   const options: editor.IStandaloneEditorConstructionOptions = {
     selectOnLineNumbers: true,
     minimap: {
